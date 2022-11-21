9b85aff398e8b434deeba553b0a983f625d7f812
Melina O'Dell <melinaodell48@gmail.com>
2022-11-21 17:25:55 -0500

    lil dark mode fixes

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,11 +3,11 @@ import React from "react"
 const Navbar = ({ darkMode, modeChangeHandler }) => (
     <div className={`ui ${darkMode ? "inverted" : ""} fixed huge menu`} style={{borderBottom: "1px solid rgba(255,255,255,.08)"}}>
         <a className="borderless item"
-                data-tooltip="Switch to light mode" 
+                data-tooltip={`Switch to ${darkMode ? "light" : "dark"} mode`} 
                 data-position="bottom left"
                 data-variation="basic"
                 onClick={modeChangeHandler}>
-            <i className="moon icon"></i>
+            <i className={`${darkMode ? "moon" : "sun"} icon`}></i>
         </a>
         <div className="right menu">
             <a href="src/assets/Resume_Melina_ODell.pdf" download 
