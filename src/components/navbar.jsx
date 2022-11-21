9faf7baad52eb425f23efe019521d8da49618f39
Melina O'Dell <melinaodell48@gmail.com>
2022-11-21 17:16:38 -0500

    Add dark/light mode toggle

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,30 +1,29 @@
 import React from "react"
 
-const Navbar = () => (
-    <div className="ui inverted segment">
-        <div className="ui inverted fixed huge menu" style={{borderBottom: "1px solid rgba(255,255,255,.08)"}}>
-            <div className="borderless item"
-                    data-tooltip="Switch to light mode" 
-                    data-position="bottom left"
-                    data-variation="basic">
-                <i className="moon icon"></i>
-            </div>
-            <div className="right menu">
-                <a href="src/assets/Resume_Melina_ODell.pdf" download 
-                    className="item" 
-                    data-tooltip="Download my resume" 
-                    data-position="bottom right"
-                    data-variation="basic">
-                        Resume
-                </a>
-                <a href="#contact" 
-                    className="item"
-                    data-tooltip="Contact me" 
-                    data-position="bottom right"
-                    data-variation="basic">
-                        Contact
-                </a>
-            </div>
+const Navbar = ({ darkMode, modeChangeHandler }) => (
+    <div className={`ui ${darkMode ? "inverted" : ""} fixed huge menu`} style={{borderBottom: "1px solid rgba(255,255,255,.08)"}}>
+        <a className="borderless item"
+                data-tooltip="Switch to light mode" 
+                data-position="bottom left"
+                data-variation="basic"
+                onClick={modeChangeHandler}>
+            <i className="moon icon"></i>
+        </a>
+        <div className="right menu">
+            <a href="src/assets/Resume_Melina_ODell.pdf" download 
+                className="item" 
+                data-tooltip="Download my resume" 
+                data-position="bottom right"
+                data-variation="basic">
+                    Resume
+            </a>
+            <a href="#contact" 
+                className="item"
+                data-tooltip="Contact me" 
+                data-position="bottom right"
+                data-variation="basic">
+                    Contact
+            </a>
         </div>
     </div>
 )
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,22 +1,28 @@
-import React from "react"
-import { Link } from "gatsby"
+import React, { useState } from "react"
+import { Link, useScrollRestoration } from "gatsby"
 import { SEO } from "../components/seo"
 import Navbar from "../components/navbar"
 
 const pageStyles = {
-  background: "#1b1c1d",
-  padding: "20px 10% 0 10%",
+  // background: "#1b1c1d",
+  padding: "60px 10% 0 10%",
   height: "100vh",
   borderRadius: "0"
 }
 
 const IndexPage = () => {
+  const [darkMode, setMode] = useState(true); // dark mode toggle; defaults to dark
+  const changeMode = () => {
+    const newMode = darkMode ? false : true;
+    setMode(newMode);
+  }
+
   return (
-    <main className="ui inverted segment" style={pageStyles}>
-      <Navbar />
-      <h1>Welcome to my Gatsby site!</h1>
-      <Link to="/posts">Posts</Link>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+    <main className={`ui ${darkMode ? "inverted" : ""} segment`} style={pageStyles}>
+      <Navbar darkMode={darkMode} modeChangeHandler={changeMode}/>
+      <h1>Melina O'Dell</h1>
+      {/* <Link to="/posts">Posts</Link> */}
+      <p>Makin' websitez</p>
     </main>
   )
 }
