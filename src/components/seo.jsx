0c06a33bf332e8f64c5b2c02bf41de22a1e169d1
Melina O'Dell <melinaodell48@gmail.com>
2022-11-21 15:28:34 -0500

    Use Fomantic UI

--- src/components/seo.jsx
+++ src/components/seo.jsx
@@ -14,6 +14,12 @@ export const SEO = ({ title, description, pathname, children }) => {
     <>
         <title>{seo.title}</title>
         <meta name="description" content={seo.description} />
+
+        {/* Fomantic UI Styles (Credit to and inspired by eecs485.org) */}
+        <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.min.js"></script>
+        <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.0/dist/semantic.min.css"/>
+        <script src="https://cdn.jsdelivr.net/npm/fomantic-ui@2.9.0/dist/semantic.min.js"></script>
+        
         {children}
     </>
   )
