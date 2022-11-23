d24f4766d0de44f3eb8d1ad5d34eb4ed97222d18
Melina O'Dell <melinaodell48@gmail.com>
2022-11-22 23:41:01 -0500

    Add bio

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -16,7 +16,13 @@ const Bio = ({ darkMode }) => (
         </div>
         <div className="column">
             <h1>Melina O'Dell</h1>
-            <p>Makin' websitez</p>
+            {/* <p>Makin' websitez</p> */}
+            <div className="ui container">
+                <p>Melina is a senior at the University of Michigan in Ann Arbor, majoring in Computer Science. 
+                    She intends to pursue a Master's degree in Computer Science Engineering through Michigan's SUGS program.
+                </p>
+                <p>She is passionate about full-stack web development, supporting women in STEM, teaching, and perpetual learning.</p>
+            </div>
         </div>
     </div>
 );
