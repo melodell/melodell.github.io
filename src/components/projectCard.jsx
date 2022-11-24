a4dad8ee3fc4fcf3d8dbfb376f21d8069df72862
Melina O'Dell <melinaodell48@gmail.com>
2022-11-24 18:05:16 -0500

    Fix card labels on mobile

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -2,12 +2,11 @@ import React from "react";
 
 const cardStyles = {
     padding: "2px",
-    paddingBottom: "30px"
 }
 
 const ProjectCard = ({ darkMode, data }) => (
     <a className={`ui ${darkMode ? "inverted" : ""} card`} href={data.url} target="_blank" style={cardStyles}>
-        <div className="content">
+        <div className="content" style={{paddingBottom: "45px"}}>
             <div className="header">{data.title}</div>
             <div className="description">{data.description}</div>
             <div className={`ui bottom attached ${data.labelColor} label`}>{data.label}</div>
