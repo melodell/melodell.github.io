b632db81fe3b0a380097df0f310241eb7af4f646
Melina O'Dell <melinaodell48@gmail.com>
2022-11-25 22:56:05 -0500

    Add tech content to project card

--- src/components/projectCard.jsx
+++ src/components/projectCard.jsx
@@ -6,13 +6,18 @@ const cardStyles = {
 
 const ProjectCard = ({ darkMode, data }) => (
     <a className={`ui ${darkMode ? "inverted" : ""} card`} href={data.url} target="_blank" style={cardStyles}>
-        <div className="content" style={{paddingBottom: "45px"}}>
+        <div className="content">
             <div className="header">{data.title}</div>
             <div className="description">{data.description}</div>
             <div className={`ui bottom attached ${data.labelColor} label`}>{data.label}</div>
             <div className={`ui right floating icon label`}>
                 <i className="black github icon"></i>
             </div>
+        </div>
+        <div className="extra content" style={{paddingBottom: "45px"}}>
+            {data.tech.map((t, i) => (
+                <span>{t}{i !== data.tech.length - 1 ? " | " : ""}</span>
+            ))}
         </div>
     </a>
 );
