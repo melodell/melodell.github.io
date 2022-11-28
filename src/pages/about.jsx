40a05ddbdbec02ea545490bdb816687819c06fe3
Melina O'Dell <melinaodell48@gmail.com>
2022-11-28 08:00:25 -0500

    Add about page

---
+++ src/pages/about.jsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+import 'fomantic-ui-css/semantic.min.css';
+
+import { Seo } from "../components/seo"
+
+const AboutPage = () => {
+  return (
+    <main>
+      <h1>Coming Soon</h1>
+    </main>
+  )
+}
+
+export default AboutPage;
+
+export const Head = () => (
+  <Seo />
+)
